4f099f0ab3d1237b159aaa7cc3035bf4cff4ff63
RomanSur1998 <123935997+RomanSur1998@users.noreply.github.com>
2023-10-30 17:42:12 +0600

    added fetch

---
+++ giphy.js
@@ -0,0 +1,35 @@
+let search = document.querySelector(".search");
+let list = document.querySelector(".list");
+
+search.addEventListener("change", () => {
+  getGiphy(search.value);
+});
+
+getGiphy();
+async function getGiphy(text) {
+  try {
+    let response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?q=${text}&api_key=YOxgnLIErg6GBhZD7dCPoEoy4XJHvFoE&limit=20`
+    );
+    let data = await response.json();
+    craeteGifList(data.data);
+    console.log(data.data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function craeteGifList(gifList) {
+  list.innerHTML = "";
+  gifList.forEach((elem) => {
+    let li = document.createElement("li");
+    let img = document.createElement("img");
+    let h3 = document.createElement("h3");
+
+    h3.textContent = elem.url;
+    img.src = elem.images.original.url;
+    li.appendChild(img);
+    list.append(li);
+  });
+}
